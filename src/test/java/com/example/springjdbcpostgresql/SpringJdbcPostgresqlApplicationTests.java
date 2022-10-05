aad69716005c971a8ed317299e5abd7738bc6574
Junho Park <dwarfthema@gmail.com>
2022-10-05 23:12:34 +0900

    init

---
+++ src/test/java/com/example/springjdbcpostgresql/SpringJdbcPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjdbcpostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcPostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
